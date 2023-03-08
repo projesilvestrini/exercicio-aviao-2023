26c2f8a25e970ee500376082e8845da3824204ce
projetoscintia <cpinho@tecnologia-unica.com.br>
2023-03-08 14:53:50 -0300

    first commit

---
+++ Exemplo.cs
@@ -0,0 +1,29 @@
+namespace exercicio_aviao_2023;
+class Exemplo
+{
+    //atributos
+    public string nome = "";
+
+    public int idade = 0;
+
+    public bool vacinado = false;
+
+//metodos
+   public void mostraMsg(){
+
+    Console.WriteLine("oi 2ffffffffffffffffffffffffffffffffff!");
+   }
+    public void mostraNome(string texto){
+        System.Console.WriteLine("Oi " + texto);
+    }
+     public string mns(){
+        return "opa bao";
+
+     }
+
+
+
+     public int somar(int a,int b){
+        return a+b;
+     }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exercicio_aviao_2023;
+class Program
+{
+    static void Main(string[] args)
+    {
+      //  Console.WriteLine("Hello");
+        //Console.ReadKey();
+
+        //instanciar um objeto apartir da class
+        Exemplo exe01 = new Exemplo();
+        exe01.mostraMsg();
+
+        exe01.mostraNome("andre");
+
+        Console.WriteLine(exe01.mns());
+
+        Console.WriteLine();
+        System.Console.WriteLine("a soma é: "+ exe01.somar(1,1));
+
+    }
+}
